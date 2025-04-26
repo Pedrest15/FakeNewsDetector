d418b1e509d2eabd8a6d9f313178e702d58b8157
rafael-pgouveia <rafael.p.gouveia.git@gmail.com>
2025-04-26 16:13:09 -0300

    feat: add vocabulary-based classifier class

---
+++ src/vocabulary_classifier/vocabulary_classifier.py
@@ -0,0 +1,27 @@
+import json
+import nltk
+from nltk import RSLPStemmer
+
+class VocabularyClassifier:
+
+    def __init__(self):
+        with open('parameters.json', 'r') as openfile:
+            json_object = json.load(openfile)
+        self.CLASSIFICATION_THRESHOLD = json_object["CLASSIFICATION_THRESHOLD"]
+        self.DICTIONARY = json_object["DICTIONARY"]
+        nltk.download('rslp')
+        self.stemmer = RSLPStemmer()
+
+    def transform(self, word):
+        return self.stemmer.stem(word)       
+
+    def news_classifier(self, sentence: str):
+        words = [self.transform(word) for word in sentence.split()]
+
+        score = 0
+        for word in words:
+            score += self.DICTIONARY.get(word,0)
+        if score > self.CLASSIFICATION_THRESHOLD:
+            return "FAKE"
+        else:
+            return "REAL"
